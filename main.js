61c43f76d6dde5b2851ca3522f5ce6e3d71202e4
Hariish N <nhariish2000@sastra.ac.in>
2021-03-11 11:57:30 +0530

    fixed stray return

--- main.js
+++ main.js
@@ -471,7 +471,7 @@ async function test (servantId, argStr, servantName) {
 		}
 
 		flatDamage += f(parseFloat(passiveSkills.flatdamage?.value ?? 0));
-		npGen += f(parseFloat(passiveSkills.npgen?.value ?? 0))/f(100); return `${npGen}`;
+		npGen += f(parseFloat(passiveSkills.npgen?.value ?? 0))/f(100);
 		npMod += f(parseFloat(passiveSkills.npmod?.value ?? 0))/f(100);
 
 		if (isCrit && faceCard) pMod += critDamage;
