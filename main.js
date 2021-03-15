5f358cc1bbdad4db5fba294d510585fd510bb9e7
Hariish N <nhariish2000@sastra.ac.in>
2021-03-15 14:58:49 +0530

    Fixed typo

--- main.js
+++ main.js
@@ -671,7 +671,7 @@ async function chain (servantId, argStr, servantName, match) {
 
 	let busterchain = cards.reduce((acc, val) => acc + val);
 
-	if (['bbnp', 'bnpb', 'npbb'].includes(busterchain) && servant.npblePhantasms[0].card === 'buster') attache += '--bbb ';
+	if (['bbnp', 'bnpb', 'npbb'].includes(busterchain) && servant.noblePhantasms[0].card === 'buster') attache += '--bbb ';
 
 	argStr = attache + argStr;
 	cards = [...cards, 'e'];
